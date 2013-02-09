f448566a2037867f88911803fba0aed71b22779e
Cathy Wu <cathywu@mit.edu>
2013-02-09 02:17:47 -0500

    basic working prototype for querying 'hotspots' (top 10) around a location query (30 mi radius)

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 import os
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+# WB_REST_API_KEY = 'fbzpn6ambx9de5nusf49rv4d'
+# WB_GEO_API_KEY = '2unavy5kacmhfh95u5cahfvw'
+# GN_USERNAME = 'ms_test201302'
+# GN_SERVER = 'ws.geonames.net'
+
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template("index.html")
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
+    app.debug = True
     app.run(host='0.0.0.0', port=port)
